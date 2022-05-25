500c6bab704cde2c363c872a42f68b698ea573de
vinielelopes <vinielelopes@outlook.com>
2022-05-25 20:52:58 -0300

    adcionando form, colocando data, alteracoes css e mais algumas finalizacoes

--- comandos.js
+++ comandos.js
@@ -1,24 +1,25 @@
 let elementDate = $("#data");
-let botao = $("#btn");
 let displayN = $("#displayNasa");
 
 
-botao.on("click", function display(){
+let pedido = (dateUsuario)=> {
     $.ajax({
-        url: "https://api.nasa.gov/planetary/apod?api_key=M2P5a7nzumVmR1clw4c37PI21eFsrW1wgVD7qDuS",
+        url: `https://api.nasa.gov/planetary/apod?api_key=M2P5a7nzumVmR1clw4c37PI21eFsrW1wgVD7qDuS&date=${dateUsuario}`,
+        type: "GET",
         success(resposta){
             let imagem = resposta.url
-            let data = resposta.date
             let tit = resposta.title
             let exp = resposta.explanation
 
+
             displayN.append(`
-                <img src = "${imagem}">
                 <h2>${tit}</h2>
+                <img src = "${imagem}">
                 <p>${exp}</p>
 
             
             `)
+
 
             let divDis = $("#displayNasa");
             divDis.css("display", "block");
@@ -27,4 +28,12 @@ botao.on("click", function display(){
         }
     });
    
+}
+
+$("#formulario").submit((event)=>{
+    event.preventDefault()
+    let dateUsuario = elementDate.val()
+    pedido(dateUsuario)
+
+
 })
