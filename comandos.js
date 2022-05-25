67135759065a2d26a56cddf44341479a6320fbd0
vinielelopes <vinielelopes@outlook.com>
2022-05-25 18:51:30 -0300

    inserindo o jquery e ajax

--- comandos.js
+++ comandos.js
@@ -1,32 +1,32 @@
-let elementImg = document.querySelector("img");
-let elementDate = document.querySelector("#data");
-let tituloFoto = document.querySelector("#titulo");
-let resumo = document.querySelector("#explicacao");
-let botao = document.querySelector("#btn");
+let elementImg = $("img");
+let elementDate = $("#data");
+let tituloFoto = $("#titulo");
+let resumo = $("#explicacao");
+let botao = $("#btn");
 
-let requisicao = new XMLHttpRequest();
-requisicao.open("GET", "https://api.nasa.gov/planetary/apod?api_key=M2P5a7nzumVmR1clw4c37PI21eFsrW1wgVD7qDuS");
-requisicao.send();
+$.ajax({
+    url: "https://api.nasa.gov/planetary/apod?api_key=M2P5a7nzumVmR1clw4c37PI21eFsrW1wgVD7qDuS",
+    success(resposta){
+        let imagem = resposta.url
+        let data = resposta.date
+        let tit = resposta.title
+        let exp = resposta.explanation
+    
+        elementImg.src = imagem
+        tituloFoto.textContent = tit
+        resumo.textContent = exp
+    
+        botao.addEventListener("click", function display(){
+            let divDis = $("#displayNasa");
+            divDis.style.display = "block";
+    
+        })
 
-requisicao.addEventListener("load", function(){
-    let resposta = JSON.parse(requisicao.responseText)
-    console.log(resposta);
-
-    let imagem = resposta.url
-    let data = resposta.date
-    let tit = resposta.title
-    let exp = resposta.explanation
-
-    elementImg.src = imagem
-    tituloFoto.textContent = tit
-    resumo.textContent = exp
-
-    botao.addEventListener("click", function display(){
-        let divDis = document.querySelector("#displayNasa");
-        divDis.style.display = "block";
-
-    })
+    }
+});
 
 
-})
+
+
+
 
