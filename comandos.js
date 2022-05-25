9fda37fc76a5fa1e832709a0106aff0fd23888d6
vinielelopes <vinielelopes@outlook.com>
2022-05-25 19:49:37 -0300

    mudando tudo pra se adaptar ao jQuery

--- comandos.js
+++ comandos.js
@@ -1,32 +1,30 @@
-let elementImg = $("img");
 let elementDate = $("#data");
-let tituloFoto = $("#titulo");
-let resumo = $("#explicacao");
 let botao = $("#btn");
+let displayN = $("#displayNasa");
 
-$.ajax({
-    url: "https://api.nasa.gov/planetary/apod?api_key=M2P5a7nzumVmR1clw4c37PI21eFsrW1wgVD7qDuS",
-    success(resposta){
-        let imagem = resposta.url
-        let data = resposta.date
-        let tit = resposta.title
-        let exp = resposta.explanation
-    
-        elementImg.src = imagem
-        tituloFoto.textContent = tit
-        resumo.textContent = exp
-    
-        botao.addEventListener("click", function display(){
+
+botao.on("click", function display(){
+    $.ajax({
+        url: "https://api.nasa.gov/planetary/apod?api_key=M2P5a7nzumVmR1clw4c37PI21eFsrW1wgVD7qDuS",
+        success(resposta){
+            let imagem = resposta.url
+            let data = resposta.date
+            let tit = resposta.title
+            let exp = resposta.explanation
+
+            displayN.append(`
+                <img src = "${imagem}">
+                <h2>${tit}</h2>
+                <p>${exp}</p>
+
+            
+            `)
+
             let divDis = $("#displayNasa");
-            divDis.style.display = "block";
+            divDis.css("display", "block");
+              
     
-        })
-
-    }
-});
-
-
-
-
-
-
+        }
+    });
+   
+})
